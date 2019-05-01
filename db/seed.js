53c1177c05317aab3dc202702864e445f3a78816
Melissa Price <melissaashprice@gmail.com>
2019-05-01 16:59:27 -0400

    setup Sneaker Schema

--- db/seed.js
+++ db/seed.js
@@ -1,3 +1,4 @@
 const Brand = require('../models/Brand');
 const Category = require('../models/Category');
 const Sneaker = require('../models/Sneaker');
+
--- models/Sneaker.js
+++ models/Sneaker.js
@@ -2,7 +2,17 @@ const mongoose = require('../db/connection');
 const Schema = mongoose.Schema;
 
 const SneakerSchema = new Schema({
-
+    name: String,
+    'release-date': String,
+    specifics: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category'
+    },
+    cost: Number,
+    links: {
+        original: String,
+        'resale-price': String
+    }
 })
 
 let sneaker = mongoose.model('Sneaker', SneakerSchema);
