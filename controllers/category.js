aebbfd6054a6fd2711a4f7e24cf7a081d77d2993
Melissa Price <melissaashprice@gmail.com>
2019-05-02 12:41:10 -0400

    setup controllers

--- controllers/category.js
+++ controllers/category.js
@@ -1,6 +1,59 @@
 const express = require('express');
 const router = express.Router();
 
+//require category model
+const Category = require('../models/Category');
+
+//read
+router.get('/', (req,res) => {
+    Category.find({}).then(allCategories => res.json(allCategories));
+});
+
+//read by type of shoe
+router.get('/:type', (req,res) => {
+    Category.findOne({type: req.params.type})
+    .then(getByType => res.json(getByType));
+});
+
+//get by _id
+router.get('/id/:_id', (req,res) => {
+    let id = req.params._id;
+    Category.find({_id: id})
+    .then(respond => res.json(respond));
+
+});
+
+//create new category
+router.post('/', (req,res) => {
+    Category.create(req.body)
+    .then(createdCategory => res.json(createdCategory));
+});
+
+//updated a category by ._id
+router.put('/update/:_id', (req,res) => {
+    let id = req.params._id;
+    Category.findOneAndUpdate({_id: id}, req.body)
+    .then(updatedCategory => res.json(updatedCategory));
+});
+
+
+//update by name
+router.put('/type/:type', (req,res) => {
+    Category.findOneAndUpdate({type: req.params.type}, req.body)
+    .then(updatedCategory => res.json(updatedCategory));
+});
+
+
+//delete by style
+router.delete('/deleted/:style', (req,res) => {
+    Category.findOneAndDelete({style: req.params.style})
+    .then(deletedCategory => res.json(deletedCategory));
+});
+
+
+
+
+
 
 
 
--- controllers/sneaker.js
+++ controllers/sneaker.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+const Sneaker = require('../models/Sneaker');
+
+//READ
+//get all sneakers
+router.get('/', (req,res) => {
+    Sneaker.find({}).then(allSneakers => res.json(allSneakers));
+});
+
+//get by name
+router.get('/:name', (req,res) => {
+    Sneaker.findOne({name: req.params.name})
+    .then(foundSneaker => res.json(foundSneaker));
+});
+//READ ENDS********
+
+
 
 
 module.exports = router;
