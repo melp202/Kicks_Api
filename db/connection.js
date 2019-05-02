6ef1985500ced060fefe65f55bcf5571249dfe4c
Melissa Price <melissaashprice@gmail.com>
2019-05-02 14:39:38 -0400

    setup heroku

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,19 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/kicks_db', { useNewUrlParser: true });
-
 mongoose.Promise = Promise;
+
+let mongoURL = "";
+
+if(process.env.NODE_ENV === 'production') {
+    mongoURL = process.env.DB_URL;
+}
+else {
+    mongoURL = 'mongodb://localhost/kicks_db';
+}
+
+mongoose.connect(mongoURL, { useNewUrlParser: true })
+.then(inst => console.log(`Connected to database: ${inst.connections[0].name}`))
+.catch(err => console.log('Connection failed!', err));
+
+
 
 module.exports = mongoose;
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const parser = require('body-parser');
-const app = express();
-
 
 //require all controllers
 const brandController = require('./controllers/brand');
 const categoryController = require('./controllers/category');
 const sneakerController = require('./controllers/sneaker');
+
+const app = express();
 
 app.use(parser.urlencoded({extended: true}));
 app.use(parser.json());
@@ -15,4 +15,7 @@ app.use('/api/brands/', brandController);
 app.use('/api/category/', categoryController);
 app.use('/api/sneakers/', sneakerController);
 
-app.listen(4080, () => console.log('Server running on port 4080!'));
+app.set('port', process.env.PORT || 4080)
+
+app.listen(app.get('port'), () =>
+console.log(`App active on port ${app.get('port')}`));
