345cb96af5ddaa0b994f85c989ace777ca062dac
Melissa Price <melissaashprice@gmail.com>
2019-05-03 10:50:53 -0400

    added redirect

--- index.js
+++ index.js
@@ -8,16 +8,23 @@ const app = express();
 const brandController = require('./controllers/brand');
 const categoryController = require('./controllers/category');
 const sneakerController = require('./controllers/sneaker');
-
+//user cors
 app.use(cors());
 
 app.use(parser.urlencoded({extended: true}));
 app.use(parser.json());
 
-
+//use all controllers
 app.use('/api/brands/', brandController);
 app.use('/api/category/', categoryController);
 app.use('/api/sneakers/', sneakerController);
+
+// add redirect to brands
+app.get('/', (req,res) => {
+    res.redirect('/api/brands');
+});
+
+
 
 app.set('port', process.env.PORT || 4080);
 
